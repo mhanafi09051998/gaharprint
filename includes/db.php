2d3a68d33b0b61332f11e27c5f2f79ad55b4d865
Muhammad Hanafi <32757433+mhanafi09051998@users.noreply.github.com>
2025-07-13 10:43:04 +0700

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 // Database configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 define('DB_USERNAME', 'root'); // Replace with your database username
-define('DB_PASSWORD', value: 'aqqttl0gesfq6e2qnajfcbiadk76rwos');     // Replace with your database password
+define('DB_PASSWORD', value: 'lsu2zms6vmaqfnnljxoiwxfj5ini5gs0');     // Replace with your database password
 define('DB_NAME', 'gaharprint'); // Replace with your database name
 
 // Create a new MySQLi object
